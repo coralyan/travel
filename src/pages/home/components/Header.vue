7ac726870ec177907261629b002ec9b4fd21c2a6
coralyan <1184046541@qq.com>
2018-11-29 17:45:07 +0800

    add homeheader

---
+++ src/pages/home/components/Header.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="header">
+    <div class="header-left">
+      <span class="iconfont back-icon">&#xe624;</span>
+    </div>
+    <div class="header-input">
+      <span class="iconfont">&#xe632;</span>
+       输入城市/景点/游玩主题
+    </div>
+    <div class="header-right">
+      城市
+      <span class="iconfont arrow-icon">&#xe64a;</span>
+    </div>
+  </div> 
+</template>
+
+<script>
+export default {
+  name: 'HomeHeader'
+}
+</script>
+
+<style scoped lang="stylus">
+  @import '~@/assets/styles/varibles.styl'
+  .header 
+    display: flex
+    height: .86rem
+    line-height: .86rem
+    color: #fff
+    background: $bgColor
+    .header-left
+      width: .64rem
+      float: left
+      text-align: center
+      .back-icon        
+        font-size: .4rem
+    .header-input
+      flex: 1
+      height: .64rem
+      line-height: .64rem
+      padding-left: .2rem
+      margin-top: .12rem
+      margin-left: .2rem
+      color: #ccc
+      background: #fff
+      border-radius: .1rem
+    .header-right
+      width: 1.24rem
+      float: right
+      text-align: center
+      .arrow-icon
+        margin-left: -.04rem
+        font-size: .24rem
+</style>
